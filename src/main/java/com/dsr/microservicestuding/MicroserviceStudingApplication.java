46a977a9157fc83f65c7466974ee8455ed045bff
duanshengrong <der996253286@163.com>
2021-03-07 22:38:03 +0800

    init project_20210307

---
+++ src/main/java/com/dsr/microservicestuding/MicroserviceStudingApplication.java
@@ -0,0 +1,13 @@
+package com.dsr.microservicestuding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStudingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceStudingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsr/microservicestuding/MicroserviceStudingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsr.microservicestuding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceStudingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
